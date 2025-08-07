26886dc612731c76e587bf4fdf690cb932e4f39d
terabaap2530 <terabaap2530@gmail.com>
2025-08-07 16:25:57 +0530

    Update gemini.py

--- gemini.py
+++ gemini.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # ðŸ”¹ Gemini API Key
-genai.configure(api_key=os.getenv("👉🏻👉🏻👉🏻👉🏻AAPKI APIKEY👈🏻👈🏻👈🏻👈🏻"))
+genai.configure(api_key=os.getenv("AIzaSyD-21LD5nMfKLnLdLrjRRitmpefIrIVnhk"))
 
 # ðŸ”¹ Permanent System Prompt
 system_prompt = """
